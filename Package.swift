7b80637ef5b97af7e1203e36e35e385b97781a2b
yhkaplan <yhkaplan@gmail.com>
2020-07-24 15:54:38 +0900

    Add Reg swift package to make dealing w/ regexes easier

--- Package.swift
+++ Package.swift
@@ -12,14 +12,12 @@ let package = Package(
             targets: ["CreditCardScanner"]),
     ],
     dependencies: [
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/yhkaplan/Reg.git", from: "0.2.5"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "CreditCardScanner",
-            dependencies: []),
+            dependencies: ["Reg"]),
         .testTarget(
             name: "CreditCardScannerTests",
             dependencies: ["CreditCardScanner"]),
